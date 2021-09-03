861144a7d7b398f5214f45cef82051eb4a2c347d
Sery0077 <sery.vlasenko.ru@gmail.com>
2021-09-03 17:42:35 +0300

    add dependencies, layouts, view pager

--- app/src/main/java/sery/vlasenko/developerslife/MainActivity.kt
+++ app/src/main/java/sery/vlasenko/developerslife/MainActivity.kt
@@ -1,11 +1,16 @@
 package sery.vlasenko.developerslife
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import sery.vlasenko.developerslife.ui.ViewPagerAdapter
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        pager.adapter = ViewPagerAdapter(this)
     }
 }
---
+++ app/src/main/java/sery/vlasenko/developerslife/ui/BestGifFragment.kt
@@ -0,0 +1,8 @@
+package sery.vlasenko.developerslife.ui
+
+import androidx.fragment.app.Fragment
+import sery.vlasenko.developerslife.R
+
+class BestGifFragment: Fragment(R.layout.item_page) {
+
+}
